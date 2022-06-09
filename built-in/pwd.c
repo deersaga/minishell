8b8c235eb7ee93ddf7b7cc7555c405c29a2fb3bd
deer <deersaga4@gmail.com>
2022-06-09 12:58:48 +0900

    refactoring

--- built-in/pwd.c
+++ built-in/pwd.c
@@ -2,17 +2,17 @@
 
 int	ft_pwd(int argc, char **argv)
 {
-	char	*curdir;
+	char	*pwd;
 
-	curdir = getcwd(NULL, 0);
-	if (!curdir)
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
 	{
 		perror("getcwd");
-		free(curdir);
+		free(pwd);
 		return (EXIT_FAILURE);
 	}
-	ft_putstr_fd(curdir, STDOUT_FILENO);
+	ft_putstr_fd(pwd, STDOUT_FILENO);
 	ft_putstr_fd("\n", STDOUT_FILENO);
-	free(curdir);
+	free(pwd);
 	return (EXIT_SUCCESS);
 }
