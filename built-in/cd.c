e78557499c445adc096d84016046db71bb5fc71e
deer <deersaga4@gmail.com>
2022-06-09 17:28:21 +0900

    cd -　にも対応完了

--- built-in/cd.c
+++ built-in/cd.c
@@ -1,34 +1,35 @@
 #include "../minishell.h"
 
-static int	get_path(char **argv, char **path)
+static int	get_path(char **argv, char **path, t_mshell *mshell)
 {
 	if (!argv[1] || !ft_strcmp(argv[1], "~"))
-		*path = getenv("HOME");
+		*path = get_env(mshell, "HOME");
 	else if (!ft_strcmp(argv[1], "-"))
 	{
-		*path = getenv("OLDPWD");
-		if (ft_strcmp(*path, ""))
+		*path = get_env(mshell, "OLDPWD");
+		if (!ft_strcmp(*path, ""))
 		{
 			ft_putstr_fd("cd: OLDPWD not set\n", STDERR_FILENO);
 			return (EXIT_FAILURE);
 		}
+		ft_putendl_fd(*path, STDOUT_FILENO);
 	}
 	else
 		*path = argv[1];
 	return (0);
 }
 
-int	ft_cd(int argc, char **argv)
+int	ft_cd(int argc, char **argv, t_mshell *mshell)
 {
 	char	*path;
-	char	*pwd;
+	char	*oldpwd;
 
 	if (argc > 2)
 		return (EXIT_FAILURE);
-	if (get_path(argv, &path))
+	if (get_path(argv, &path, mshell))
 		return (EXIT_FAILURE);
-	pwd = getcwd(NULL, 0);
-	if (!pwd)
+	oldpwd = getcwd(NULL, 0);
+	if (!oldpwd)
 	{
 		perror("getcwd");
 		return (EXIT_FAILURE);
@@ -36,10 +37,10 @@ int	ft_cd(int argc, char **argv)
 	if (chdir(path) == -1)
 	{
 		perror("cd");
-		free(pwd);
+		free(oldpwd);
 		return (EXIT_FAILURE);
 	}
-	//ここでOLDPWDという環境変数にpwdをvalueとして登録する。
-	free(pwd);
+	register_or_update_env(mshell, "OLDPWD", oldpwd);
+	free(oldpwd);
 	return (EXIT_SUCCESS);
 }
