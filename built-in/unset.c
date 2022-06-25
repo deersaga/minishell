a4c18e7403e7eeedb208e8254bceb8e73b94a6a7
deer <deersaga4@gmail.com>
2022-06-26 05:33:18 +0900

    引数を変更しそれに対応

--- built-in/unset.c
+++ built-in/unset.c
@@ -1,14 +1,17 @@
 #include "../minishell.h"
 
-int	ft_unset(int argc, char **argv, t_mshell *mshell)
+int	ft_unset(t_mshell *mshell, t_command *cmd)
 {
 	size_t	i;
+	char	**argv;
 
 	i = 1;
+	argv = create_argv(mshell, cmd);
 	while (argv[i])
 	{
 		delete_one_env(mshell, argv[i]);
 		i++;
 	}
+	free_array(argv);
 	return (0);
 }
