641058f44b56dcf883fce9a13b7c341bb1140f6c
deer <deersaga4@gmail.com>
2022-06-26 05:08:51 +0900

    引数をt_mshellとt_commandに変えた その上で対応していなかった-nnnnnのようなオプションにも対応

--- built-in/echo.c
+++ built-in/echo.c
@@ -1,30 +1,52 @@
 #include "../minishell.h"
 
-int	ft_echo(int argc, char **argv, t_mshell *mshell)
+static int	is_all_n(char *s)
+{
+	size_t	i;
+
+	i = 1;
+	if (!s[1])
+		return (0);
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_echo(t_mshell *mshell, t_command *cmd)
 {
 	int		n_opt;
+	char	**argv;
 	size_t	i;
+	size_t	j;
 
 	n_opt = 0;
 	i = 1;
+	argv = create_argv(mshell, cmd);
 	if (!argv[1])
 		write(STDOUT_FILENO, "\n", 1);
 	else
 	{
-		while (!ft_strcmp(argv[i], "-n"))
+		while (*argv[i] ==  '-')
 		{
+			if (!is_all_n(argv[i]))
+				break;
 			n_opt = 1;
 			i++;
 		}
-		while (i < argc)
+		while (i < cmd->argc)
 		{
 			ft_putstr_fd(argv[i], STDOUT_FILENO);
-			if (i != argc - 1)
+			if (i != cmd->argc - 1)
 				write(STDOUT_FILENO, " ", 1);
 			i++;
 		}
 		if (!n_opt)
 			write(STDOUT_FILENO, "\n", 1);
 	}
+	free_array(argv);
 	return (EXIT_SUCCESS);
 }
