0de4a716cb5606911f938dc2775d28fbc9f5a813
deer <deersaga4@gmail.com>
2022-06-29 02:52:01 +0900

    exit.c完成

--- built-in/exit.c
+++ built-in/exit.c
@@ -1,6 +1,31 @@
 #include "../minishell.h"
 
+static void	msg_exit(int status)
+{
+	ft_putstr_fd("exit\n", 2);
+	exit(status);
+}
+
 int	ft_exit(t_mshell *mshell, t_command *cmd)
 {
-	return (0);
+	long	status;
+
+	create_argv(mshell, cmd);
+	if (cmd->argc > 2)
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		return (1);
+	}
+	else if (cmd->argc == 2)
+	{
+		if (all_signed_num(cmd->argv[1]))
+		{
+			status = ft_atol(cmd->argv[1]);
+			msg_exit(status % 256);
+		}
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		msg_exit(255);
+	}
+	msg_exit(EXIT_SUCCESS);
+	return(0);
 }
