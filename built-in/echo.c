af38fec19bf3c38b36320c5d5f1e372d1b01e4e1
ktada42 <kaou.tad.147.258.369@gmail.com>
2022-07-01 00:20:56 +0900

    test

--- built-in/echo.c
+++ built-in/echo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 12:26:13 by katakagi          #+#    #+#             */
+/*   Updated: 2022/06/29 12:26:14 by katakagi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 static int	is_all_n(char *s)
@@ -16,27 +28,33 @@ static int	is_all_n(char *s)
 	return (1);
 }
 
+static int	check_option(char **argv, size_t *i)
+{
+	int	n_opt;
+
+	n_opt = 0;
+	while (argv[*i] && *(argv[*i]) == '-')
+	{
+		if (!is_all_n(argv[*i]))
+			break ;
+		n_opt = 1;
+		(*i)++;
+	}
+	return (n_opt);
+}
+
 int	ft_echo(t_mshell *mshell, t_command *cmd)
 {
 	int		n_opt;
 	size_t	i;
-	size_t	j;
 
 	(void)mshell;
 	n_opt = 0;
 	i = 1;
 	create_argv(mshell, cmd);
-	if (!cmd->argv[1])
-		write(STDOUT_FILENO, "\n", 1);
-	else
+	if (cmd->argv[1])
 	{
-		while (*(cmd->argv[i]) ==  '-')
-		{
-			if (!is_all_n(cmd->argv[i]))
-				break;
-			n_opt = 1;
-			i++;
-		}
+		n_opt = check_option(cmd->argv, &i);
 		while (i < cmd->argc)
 		{
 			ft_putstr_fd(cmd->argv[i], STDOUT_FILENO);
@@ -44,9 +62,8 @@ int	ft_echo(t_mshell *mshell, t_command *cmd)
 				write(STDOUT_FILENO, " ", 1);
 			i++;
 		}
-		if (!n_opt)
-			write(STDOUT_FILENO, "\n", 1);
 	}
-	//free_array(cmd->argv);
+	if (!n_opt)
+		write(STDOUT_FILENO, "\n", 1);
 	return (EXIT_SUCCESS);
 }
--- built-in/export.c
+++ built-in/export.c
@@ -1,6 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 13:39:19 by katakagi          #+#    #+#             */
+/*   Updated: 2022/06/29 13:44:28 by katakagi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-int	is_valid(char *s)
+void	print_export(t_envList *env)
+{
+	t_envList	*cur;
+	char		*val;
+
+	(void)val;
+	cur = env;
+	while (cur->key)
+	{
+		printf("declare -x %s", cur->key);
+		if (cur->val)
+			printf("=\"%s\"", cur->val);
+		printf("\n");
+		cur = cur->next;
+	}
+}
+
+void	get_key_val(char *key_eq_val, char *key_val[2])
+{
+	char	*eq;
+
+	eq = ft_strchr(key_eq_val, '=');
+	if (!eq)
+	{
+		key_val[0] = ft_strdup(key_eq_val);
+		key_val[1] = NULL;
+	}
+	else
+	{
+		key_val[0] = ft_substr(key_eq_val, 0, eq - key_eq_val);
+		key_val[1] = ft_substr(eq + 1, 0, ft_strlen(key_eq_val));
+	}
+}
+
+static int	is_valid(char *s)
 {
 	char	*eq_pos;
 	char	*cur;
@@ -18,61 +64,54 @@ int	is_valid(char *s)
 	return (1);
 }
 
+static void	create_export_argv(t_mshell *mshell, t_command *cmd)
+{
+	size_t	len;
+	t_token	*cur;
+
+	create_argv(mshell, cmd);
+	len = 0;
+	while (cmd->argv[len])
+		len++;
+	free_array(cmd->argv);
+	cmd->argv = ft_calloc(len + 1, sizeof(char *));
+	len = 0;
+	cur = cmd->token;
+	while (1)
+	{
+		cur = skip_delimiter_token(cur);
+		if (cur && cur->type == T_END)
+			break ;
+		cmd->argv[len++] = concat_expanded_tokens(mshell, cur);
+		cur = skip_by_next_delimiter_token(cur);
+	}
+	cmd->argv[len] = NULL;
+}
 
 int	ft_export(t_mshell *mshell, t_command *cmd)
 {
 	char		*eq_pos;
-	char		*key;
-	char		*val;
+	char		*key_val[2];
 	size_t		i;
 
-	
-	create_argv(mshell, cmd);
+	create_export_argv(mshell, cmd);
 	if (cmd->argc == 1)
 	{
 		sort_env(mshell->env);
-		print_env(mshell->env);
+		print_export(mshell->env);
 	}
 	i = 0;
 	while (cmd->argv[++i])
-	{
+	{		
 		if (!is_valid(cmd->argv[i]))
 		{
 			ft_putstr_fd("export: not a valid identifier\n", 2);
-			continue;
-		}		
-		eq_pos = ft_strchr(cmd->argv[i], '=');
-		if (!eq_pos)
-		{
-			key = ft_strdup(cmd->argv[i]);
-			val = NULL;
+			continue ;
 		}
-		else
-		{
-			key = ft_substr(cmd->argv[i], 0, eq_pos - cmd->argv[i]);
-			val = ft_substr(eq_pos + 1, 0, ft_strlen(cmd->argv[i]));
-		}
-		register_or_update_env(mshell, key, val);
-		free(key);
-		free(val);
+		get_key_val(cmd->argv[i], key_val);
+		register_or_update_env(mshell, key_val[0], key_val[1]);
+		free(key_val[0]);
+		free(key_val[1]);
 	}
-	free_array(cmd->argv);
 	return (0);
 }
-
-/*
-int main(int argc, char **argv)
-{
-	t_mshell mshell;
-
-	init_env(&mshell);
-	ft_export(1, argv, &mshell);
-	//printf("%s\n", tmp);
-	//print_env(mshell.env);
-	delete_all_env(&mshell);
-}
-
-__attribute__((destructor)) static void destructor()
-{
-	system("leaks -q a.out");
-}*/
