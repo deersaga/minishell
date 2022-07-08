4d0b334f42ae5b58886c6ca521e3fafe46b53410
deer <deersaga4@gmail.com>
2022-07-08 19:43:32 +0900

    open_file.c内の関数をexecuter/redirect.cに移動してfile directoryを削除した

--- executer/redirect.c
+++ executer/redirect.c
@@ -6,11 +6,35 @@
 /*   By: katakagi <katakagi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 16:31:36 by kaou              #+#    #+#             */
-/*   Updated: 2022/07/07 21:04:00 by katakagi         ###   ########.fr       */
+/*   Updated: 2022/07/08 19:42:35 by katakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	openfile(char *filename, t_type_token mode)
+{
+	int	fd;
+
+	if (mode == T_REDIR_IN)
+	{
+		if (access(filename, F_OK))
+		{
+			fprintf(stderr, "%s: No such file or directory\n", filename);
+			exit(EXIT_FAILURE);
+		}
+		fd = (open(filename, O_RDONLY));
+	}
+	else if (mode == T_REDIR_OUT)
+		fd = (open(filename, O_CREAT | O_WRONLY | O_TRUNC, \
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
+	else
+		fd = (open(filename, O_CREAT | O_WRONLY | O_APPEND, \
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
+	if (fd < 0)
+		fprintf(stderr, "%s: No such file or directory\n", filename);
+	return (fd);
+}
 
 int	reconnect_redir_with_stdio(t_command *cur_com)
 {
